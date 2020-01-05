1c97d7a8233f0d750115bc3e4f58bb7856057368
Cees-Jan Kiewiet <ceesjank@gmail.com>
2020-01-05 21:40:50 +0100

    Removed debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,6 @@
 mkdir ~/.kube/
 echo "${KUBECONFIG_FILE_CONTENTS}" > ~/.kube/config
 echo "helm ${INPUT_ARGS}" > /tmp/run.sh
-cat /tmp/run.sh
-ls -lasth
 chmod +x /tmp/run.sh
 /tmp/run.sh
 rm /tmp/run.sh

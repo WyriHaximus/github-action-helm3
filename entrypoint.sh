ad0ed4eb8f5cd4569ed9b1417becf7830aab1df0
Cees-Jan Kiewiet <ceesjank@gmail.com>
2020-01-05 21:33:19 +0100

    chmod not chod

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ mkdir ~/.kube/
 echo "${KUBECONFIG_FILE_CONTENTS}" > ~/.kube/config
 echo "helm ${INPUT_ARGS}" > /tmp/run.sh
 cat /tmp/run.sh
-chod +x /tmp/run.sh
+chmod +x /tmp/run.sh
 /tmp/run.sh
 rm /tmp/run.sh

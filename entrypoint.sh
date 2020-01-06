f8b4680fd550beb753b2e183f3444b325093e107
Cees-Jan Kiewiet <ceesjank@gmail.com>
2020-01-06 10:47:34 +0100

    Logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+echo -e "\033[36mSetting up kubectl configuration\033[0m"
 mkdir ~/.kube/
 echo "${KUBECONFIG_FILE_CONTENTS}" > ~/.kube/config
+
+echo -e "\033[36mPreparing helm execution\033[0m"
 echo "${INPUT_EXEC}" > run.sh
-chod +x ./run.sh
+chmod +x ./run.sh
+
+echo -e "\033[36mExecuting helm\033[0m"
 ./run.sh
-rm ./run.sh
+
+echo -e "\033[36mCleaning up\033[0m"
+rm ./run.sh ~/.kube/ -Rf

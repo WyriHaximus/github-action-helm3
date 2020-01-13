fa5af2c3fa466dbeaf6aeff1368407e67f240b1c
Cees-Jan Kiewiet <ceesjank@gmail.com>
2020-01-13 21:46:54 +0100

    Simplify execution

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,8 @@ echo -e "\033[36mSetting up kubectl configuration\033[0m"
 mkdir ~/.kube/
 echo "${KUBECONFIG_FILE_CONTENTS}" > ~/.kube/config
 
-echo -e "\033[36mPreparing helm execution\033[0m"
-echo "${INPUT_EXEC}" > run.sh
-chmod +x ./run.sh
-
 echo -e "\033[36mExecuting helm\033[0m"
-./run.sh
+sh ${INPUT_EXEC}
 
 echo -e "\033[36mCleaning up\033[0m"
-rm ./run.sh ~/.kube/ -Rf
+rm ~/.kube/ -Rf

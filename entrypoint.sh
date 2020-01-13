bb8a37405a46123e2243a3815a8548ed44c33474
Cees-Jan Kiewiet <ceesjank@gmail.com>
2020-01-13 22:02:13 +0100

    Revert and prepend run.sh with #!/bin/sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,12 @@ echo -e "\033[36mSetting up kubectl configuration\033[0m"
 mkdir ~/.kube/
 echo "${KUBECONFIG_FILE_CONTENTS}" > ~/.kube/config
 
+echo -e "\033[36mPreparing helm execution\033[0m"
+echo -e "#!/bin/sh\r\n\r\n${INPUT_EXEC}" > run.sh
+chmod +x ./run.sh
+
 echo -e "\033[36mExecuting helm\033[0m"
-sh ${INPUT_EXEC}
+./run.sh
 
 echo -e "\033[36mCleaning up\033[0m"
-rm ~/.kube/ -Rf
+rm ./run.sh ~/.kube/ -Rf

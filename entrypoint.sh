faf8dd4d3abf7cbaaae709dc25b2b48f040bfc26
John Renne <john.renne@getbux.com>
2022-12-07 19:31:02 +0100

    Updated node to 16 and removed deprecated method

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,7 +24,7 @@ helm_output="${helm_output//'%'/'%25'}"
 helm_output="${helm_output//$'\n'/'%0A'}"
 helm_output="${helm_output//$'\r'/'%0D'}"
 
-echo "::set-output name=helm_output::$helm_output"
+echo "helm_output=$helm_output" >> $GITHUB_OUTPUT
 
 echo -e "\033[36mCleaning up: \033[0m"
 rm ./run.sh -Rf
